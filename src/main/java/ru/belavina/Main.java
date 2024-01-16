0ac8109b7812f7365dca21d3bb8e188c52dbced0
ElenaBelavina <154471636+ElenaBelavina@users.noreply.github.com>
2024-01-17 02:16:59 +0300

    Update Main.java

--- src/main/java/ru/belavina/Main.java
+++ src/main/java/ru/belavina/Main.java
@@ -3,6 +3,6 @@ package ru.belavina;
 public class Main {
     private static String msg("Hello qa_guru!")
     public static void main(String[] args) {
-        System.out.println("msg);
+        System.out.println(msg);
     }
 }
